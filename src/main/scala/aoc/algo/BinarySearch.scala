9f9b06c5a214ab704581f539b467aab4d1cbcb26
Stoeffel <schtoeffel@gmail.com>
2023-12-06 13:39:16 +0100

    Generic binarySearch

--- src/main/scala/aoc/algo/BinarySearch.scala
+++ src/main/scala/aoc/algo/BinarySearch.scala
@@ -1,12 +1,13 @@
 package aoc.algo.BinarySearch
 
 object all:
-  extension (x: Long)
-    def binarySearch(predicate: Long => Boolean): Long =
-      def go(low: Long, high: Long): Long =
-        if low > high then low
-        else
-          val mid = (low + high) / 2
-          if predicate(mid) then go(low, mid - 1)
-          else go(mid + 1, high)
-      go(0, x)
+  extension [A: Integral](x: A)
+    def binarySearch(predicate: A => Boolean)(using
+        A: Integral[A]
+    ): A =
+      import A._
+      var (min, max) = (zero, x)
+      while (min < max)
+        val mid = (min + max) / fromInt(2)
+        if predicate(mid) then max = mid else min = mid + one
+      min
